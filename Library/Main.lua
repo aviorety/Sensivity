cb841de8fc8539157c62b06dc7bc6d2bc4f4d992
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-14 17:20:09 +0300

    t

--- Library/Main.lua
+++ Library/Main.lua
@@ -1,1 +1,82 @@
+local UserInputService = game:GetService('UserInputService')
+local TweenService = game:GetService('TweenService')
+local CoreGui = game:GetService('CoreGui')
 
+local Library = {}
+Library.flags = {}
+Library.connections = {}
+
+Library.container = nil
+Library.container_open = true
+Library.keybind = Enum.KeyCode.Insert
+
+
+function Library:clear()
+    for _, object in CoreGui:GetChildren() do
+        if object.Name ~= 'Sensivity' then
+            continue
+        end
+
+        object:Destroy()
+    end
+end
+
+
+function Library:open()
+    TweenService:Create(self.Container, TweenInfo.new(1, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        Size = UDim2.new(0, 707, 0, 390)
+    }):Play()
+end
+
+
+function Library:close()
+    TweenService:Create(self.Container, TweenInfo.new(1, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        Size = UDim2.new(0, 0, 0, 0)
+    }):Play()
+end
+
+
+function Library:init()
+    Library.clear()
+
+    local container = game:GetObjects('rbxassetid://17487656336')[1]
+    container.Parent = CoreGui
+    container.Container.Size = UDim2.new(0, 0, 0, 0)
+
+    Library.container = container
+    Library.open(container)
+
+    Library.connections['open'] = UserInputService.InputBegan:Connect(function(input: InputObject, process: boolean)
+        if process then
+            return
+        end
+
+        if input.KeyCode ~= Library.keybind then
+            return
+        end
+
+        if not Library.container.Parent then
+            if Library.connections['open'] then
+                Library.connections['open']:Disconnect()
+                Library.connections['open'] = nil
+            end
+
+            return
+        end
+
+        Library.container_open = not Library.container_open
+
+        if Library.container_open then
+            Library.open(container)
+        else
+            Library.close(container)
+        end
+    end)
+end
+
+
+--[[
+local main = Library.init()
+]]
+
+return Library
