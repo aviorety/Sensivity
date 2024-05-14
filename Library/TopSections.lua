5a893bc51de7a5479489f6074d382f154e98ec5d
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-14 19:58:31 +0300

    t

--- Library/TopSections.lua
+++ Library/TopSections.lua
@@ -1,8 +1,32 @@
+local TweenService = game:GetService('TweenService')
+
 local TopSections = {}
 TopSections.assets = {
     top_sections = game:GetObjects('rbxassetid://17492980293')[1],
     section = game:GetObjects('rbxassetid://17493046479')[1]
 }
+
+
+function TopSections:open()
+    TweenService:Create(self, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        BackgroundTransparency = 0
+    }):Play()
+
+    TweenService:Create(self, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        TextTransparency = 0
+    }):Play()
+end
+
+
+function TopSections:close()
+    TweenService:Create(self, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        BackgroundTransparency = 1
+    }):Play()
+
+    TweenService:Create(self, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        TextTransparency = 0.5
+    }):Play()
+end
 
 
 function TopSections:update()
@@ -24,6 +48,12 @@ end
 
 function TopSections:create()
     local section = TopSections.assets.section:Clone()
+    section.SectionName.Text = self.name
+
+    if not self.top_sections:FindFirstChild('Section') then
+        TopSections:open(section)
+    end
+    
     section.Parent = self.top_sections
 end
 
