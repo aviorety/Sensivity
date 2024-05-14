669b2939f3dfcb078c05feb5653e89f5093a047d
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-14 19:31:53 +0300

    t

--- Library/TopSection.lua
+++ Library/TopSection.lua
@@ -1,8 +1,9 @@
 local TopSection = {}
+TopSection.asset = game:GetObjects('rbxassetid://17493046479')[1]
 
 
 function TopSection:create()
-    local section = game:GetObjects('rbxassetid://17493046479')[1]
+    local section = TopSection.asset:Clone()
     section.Parent = self.top_sections
 end
 
