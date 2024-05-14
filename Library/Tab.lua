c9fda84281ac0c9a644314307bd938098078b725
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-14 18:00:01 +0300

    t

---
+++ Library/Tab.lua
@@ -0,0 +1,68 @@
+local TweenService = game:GetService('TweenService')
+
+local Tab = {}
+
+
+function Tab:open()
+    TweenService:Create(self.IconBackground.ImageTransparency, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        ImageTransparency = 0
+    }):Play()
+
+    TweenService:Create(self.IconBackground.Icon.ImageTransparency, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        ImageTransparency = 0
+    }):Play()
+
+    TweenService:Create(self.TabName, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        TextTransparency = 0
+    }):Play()
+end
+
+
+function Tab:close()
+    TweenService:Create(self.IconBackground.ImageTransparency, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        ImageTransparency = 1
+    }):Play()
+
+    TweenService:Create(self.IconBackground.Icon.ImageTransparency, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        ImageTransparency = 0.5
+    }):Play()
+
+    TweenService:Create(self.TabName, TweenInfo.new(0.5, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+        TextTransparency = 0.5
+    }):Play()
+end
+
+
+function Tab:update()
+    for _, object in self.tabs:GetChildren() do
+        if object.Name ~= 'Tab' then
+            continue
+        end
+
+        if object ~= self.tab then
+            Tab.close(object)
+
+            continue
+        end
+
+        Tab.open(object)
+    end
+end
+
+
+function Tab:create()
+    local tab = game:GetObjects('rbxassetid://17492320826')[1]
+    tab.Parent = self.tabs
+    tab.TabName = self.Name
+    tab.IconBackground.Icon.Image = self.icon
+
+    tab.MouseButton1Click:Connect(function()
+        Tab.update({
+            tabs = self.tabs,
+            tab = tab
+        })
+    end)
+end
+
+
+return Tab
